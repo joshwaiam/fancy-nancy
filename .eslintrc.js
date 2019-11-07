ae52615673e18782eeb58568d62e8aebd356aef2
Josh Payette <Josh@RFSI.local>
2019-11-07 11:31:59 -0500

    Fixed linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,17 @@ module.exports = {
     node: true,
     es6: true
   },
-  extends: ["airbnb", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint"],
   settings: {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"]
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import * as SlackBot from 'slackbots';
-import * as dotenv from 'dotenv';
-import * as puppeteer from 'puppeteer';
-import logger from './logger';
+import * as SlackBot from "slackbots";
+import * as dotenv from "dotenv";
+import * as puppeteer from "puppeteer";
+import logger from "./logger";
 
 dotenv.config();
 
@@ -17,22 +17,27 @@ bot.on("start", async () => {
   try {
     browser = await puppeteer.launch();
     page = await browser.newPage();
-  
+
     await page.goto(process.env.PRINTER_URL);
-  } catch(e) {
+  } catch (e) {
     logger.error(e.message);
     return;
   }
 
   const errorText = await page.evaluate(() => {
-    const el = document.getElementById("deviceErrorInfoModule").getElementsByTagName('p')[0];
-    if(!el) {
+    const el = document
+      .getElementById("deviceErrorInfoModule")
+      .getElementsByTagName("p")[0];
+    if (!el) {
       return;
     }
     return el.textContent;
   });
 
-  bot.postMessageToChannel(process.env.SLACK_CHANNEL, `PrinterBot is now online.\n\nCurrent error status:\n *${errorText}*`) 
+  bot.postMessageToChannel(
+    process.env.SLACK_CHANNEL,
+    `PrinterBot is now online.\n\nCurrent error status:\n *${errorText}*`
+  );
 
   browser.close();
 });
